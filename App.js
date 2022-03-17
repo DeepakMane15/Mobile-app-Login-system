0e4544e64de2bce60016814856b6873a9f6761cd
synccitsolution3 <synccitsolution3@gmail.com>
2022-03-17 10:31:23 +0530

    final changes

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { AuthScreen } from './screens';
+import { Authenticate } from './components';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <AuthScreen />
+      <Authenticate />
       <StatusBar style="auto" />
     </View>
   );
